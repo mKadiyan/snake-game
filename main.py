fc581deda44192075e284c12f94538700b8b338c
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2025-08-20 14:41:28 +0530

    initial structure

---
+++ main.py
@@ -0,0 +1,37 @@
+import time
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0,0),(-20,0),(-40,0)]
+segments = []
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(.1)
+
+    for seg_num in range(len(segments) -1, 0, -1):
+        nex_x = segments[seg_num - 1].xcor()
+        nex_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(nex_x, nex_y)
+    segments[0].forward(20)
+
+
+
+
+
+
+screen.exitonclick()
